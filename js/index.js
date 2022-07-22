7b20c5ee95252b769a2d4831d51d0016f9225ae3
kimddddh98 <104114436+kimddddh98@users.noreply.github.com>
2022-07-22 11:45:43 +0900

    0722

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(function(){
     }
  
     function book(){
-        $('#book_con').animate({marginLeft:-510},10000,function(){
+        $('#book_con').animate({marginLeft:-510},10000,"linear",function(){
             $('#book_con>div:first').appendTo($('#book_con'));
             $('#book_con').css({marginLeft:0})
         })
@@ -196,15 +196,40 @@ $(function(){
             $('#new>ul>li').eq(i).find('span').text(data.documents[i].publisher)
         }
     })
-    $.ajax({
-        url:"https://dapi.kakao.com/v2/search/image",
-        method:"GET",
-        data:{query:"유발하라리",size:7},
-        headers:{Authorization: "KakaoAK cf6c35b551fb1ae4f68a9f154d6f8b42"}
-    })
-    .done(function(data){
-        console.log(data)
-    })
+    const authorsCon=document.getElementById('authors_con')
+    const authorsItem=$('#authors_con>div')
+    authorsCon.style.width=authorsItem.outerWidth(true)*authorsItem.length+'px'
+    function authorsLeft(){
+        $('#authors_con').animate({marginLeft:-authorsItem.outerWidth(true)},3000,"linear",function(){
+            $('#authors_con>div:first').appendTo('#authors_con')
+            $('#authors_con').css({marginLeft:0})
+        })
+    }
+    authorsLeft();
+    setInterval(authorsLeft,3000)
+    const authors=[
+        {p:"유발하라리",span:"사피엔스",src:"./img/authors1.png"},
+        {p:"이선재",span:"국어",src:"./img/authors2.png"},
+        {p:"김중규",span:"행정학",src:"./img/authors3.png"},
+        {p:"민준호",span:"사회",src:"./img/authors4.png"},
+        {p:"이동기",span:"영어",src:"./img/authors5.png"},
+        {p:"문동균",span:"한국사",src:"./img/authors6.png"},
+        {p:"고혜원",span:"국어",src:"./img/authors7.png"},
+        {p:"신영식",span:"한국사",src:"./img/authors8.png"},
+        {p:"말콤글래드웰",span:"타인의 해석",src:"./img/authors9.png"},
+        {p:"정세랑",span:"시선으로부터",src:"./img/authors10.png"},
+        {p:"김연수",span:"일곱해의 마지막",src:"./img/authors11.png"},
+        {p:"정재승",span:"열두 발자국",src:"./img/authors12.png"},
+        {p:"백희나",span:"알사탕",src:"./img/authors13.png"},
+        {p:"김미경",span:"김미경의 리부트",src:"./img/authors14.png"},
+    ]
+    for(let i in authors){
+        $('#authors_con>div').eq(i).append(
+            $(`<img src="">`).prop('src',authors[i].src),
+            $('<p/>').text(authors[i].p),
+            $('<span/>').text(authors[i].span)
+        )
+    }
     const popupColor=['#8044ff','#cbe2cf','#c0e8ff']
     let popupIndex=0
     $('#popup ul').click(function(){
