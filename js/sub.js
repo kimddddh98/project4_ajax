d352dda477a8dff9b63bdee77258ad13545ea9af
kimddddh98 <104114436+kimddddh98@users.noreply.github.com>
2022-07-27 13:19:34 +0900

    0727

--- js/sub.js
+++ js/sub.js
@@ -1,5 +1,6 @@
 $(function(){
     $('#open img').click(function(){
+        $(this).hide()
         $('#left_menu').show()
         $('#left_menu').animate({left:0})
     })
@@ -7,6 +8,7 @@ $(function(){
         $('#left_menu').animate({left:-200},function(){
             $('#left_menu').hide()
         })
+        $('#open img').show();
     })
     $('#left_main>li').click(function(){
         $(this).siblings().children('ul').slideUp()
@@ -123,9 +125,12 @@ $(function(){
     .done(function(data){
         for(let i=0; i<$('#ad_two .ad_right>div>div').length;i++){
         $('#ad_two .ad_right>div>div').eq(i).css({backgroundImage:`url(${data.documents[i].thumbnail})`})
+        data.documents[i].title.length>23?
+        $('#ad_two .ad_right>div>p').eq(i).text(data.documents[i].title.slice(0,23)+'...'):
         $('#ad_two .ad_right>div>p').eq(i).text(data.documents[i].title);
         $('#ad_two .ad_right>div>h4').eq(i).text(`${data.documents[i].sale_price} 원`);
-        $('#ad_two .ad_right>div>h4').eq(i).text(`${data.documents[i].sale_price} 원`);
+        $('#ad_two .ad_right>div>span').eq(i).text(`${(data.documents[i].price-data.documents[i].sale_price)/(data.documents[i].price/100)}%`)
+        $('#ad_two .ad_right input').eq(i).val(data.documents[i].sale_price)
         }
     })
     // ㅡㅡㅡ텍스트 불러오기
@@ -170,5 +175,72 @@ $(function(){
         $('#info_main_review').show();
 
     })
-    
+    const review = [
+        [
+            { title: "좋은책 감사합니다", id: "c16165017954***", date: "2022-07-26", src: "../img/sub/review1.jpeg", comment: "완강을 위해 달려보겠습니다^_^" },
+            { title: "2023 이동기 영어 기본서", id: "dkes***", date: "2022-07-26", src: "../img/sub/review2.jpg", comment: "이 교재로 열심히 공부 하겠습니다." },
+            { title: "좋아요", id: "ksin5***", date: "2022-07-26", src: null, comment: "빠른배송으로 잘 도착했습니다~ 감사합니다!" },
+            { title: "마음에 들어요 추천합니다", id: "danj***", date: "2022-07-26", src: "../img/sub/review4.jpg", comment: "마음에 들어요 추천합니다" },
+            { title: "이동기 강사님 영어 기본서", id: "c16514613985***", date: "2022-07-26", src: "../img/sub/review5.jpeg", comment: "이동기 강사님 영어 기본서로 2023 합격까지~!!" }
+        ],
+        [
+            { title: "열심히 공부할게용", id: "c16576924425***", date: "2022-07-25", src: "../img/sub/review6.jpeg", comment: "분철도 깔끔하고 책도 깔끔해서 왔습니다! 열심히 공부하겠습니다~" },
+            { title: "좋아요", id: "c16441303214***", date: "2022-07-25", src: "../img/sub/review7.jpeg", comment: "배송도 빠르고 깔끔하고 좋습니다." },
+            { title: "굿굿", id: "c16561130781***", date: "2022-07-24", src: null, comment: "안에 내용들도 공부하기 쉽고 좋네요" },
+            { title: "영어는 역시 동기쌤", id: "c16572532562***", date: "2022-07-23", src: "../img/sub/review9.jpg", comment: "진짜 공시에서 유명한 동기쌤 올인원 역시 그냥 유명한게 아니에요!! 진짜 추천" },
+            { title: "깔끔해요", id: "c16532723502***", date: "2022-07-22", src: null, comment: "책이 깔끔하고 배송도 빨라요" }
+        ],
+        [
+            { title: "좋아요 열심히 공부하겠습니다.", id: "qhd***", date: "2022-07-22", src: "../img/sub/review11.jpg", comment: "좋아요 열심히 공부하겠습니다." },
+            { title: "좋아요!", id: "c16575052123***", date: "2022-07-22", src: "../img/sub/review12.jpg", comment: "분철해서 편하고, 내용도 좋아요" },
+            { title: "영어", id: "c16203489587***", date: "2022-07-22", src: "../img/sub/review13.jpeg", comment: "영단어까지 분철 되어 넘 좋고 열심히 공부하겠습니다!" },
+            { title: "이동기 영어 기본서 리뷰", id: "c16580409642***", date: "2022-07-22", src: "../img/sub/review14.jpeg", comment: "분철 깔끔하고 좋아요!! 열심히 공부할게요~!!" },
+            { title: "좋아요!", id: "c16568462237***", date: "2022-07-22", src: "../img/sub/review15.jpeg", comment: "철도 깔끔하고 배송도 빠르게 와서 좋았어요~" }
+        ],
+        [
+            { title: "이동기 올인원", id: "c16103390970***", date: "2022-07-21", src: "../img/sub/review16.jpeg", comment: "문법 독해 단어까지 다 들어가 있어서 좋습니다 :)" },
+            { title: "디자인이 깔끔하고 좋아요.", id: "chouw***", date: "2022-07-21", src: null, comment: "공부가 더 잘 될 것 같아요." },
+            { title: "좋아요", id: "c16139685599***", date: "2022-07-21", src: "../img/sub/review18.jpg", comment: "문제도 좋고 두께도 적당합니다." },
+            { title: "영어", id: "c16572918187***", date: "2022-07-21", src: "../img/sub/review19.jpg", comment: "교재가 얇아서 분철안해도 굿!! 잘 사용하겠습니다~" },
+            { title: "리뷰", id: "c16346431156***", date: "2022-07-20", src: "../img/sub/review20.jpeg", comment: "배송도 빠르고 분철 깔끔해요 책 좋아요 ㅎㅎ" }
+        ]
+    ]
+    for (let x = 0; x < $('.page_number').length; x++) {
+        $('.page_number').eq(x).on('click', function () {
+            for (let i = 0; i < $('.review_item').length; i++) {
+                $('.review_item>h3').eq(i).text(review[x][i].title)
+                $('.review_item>.review_id').eq(i).text(`${review[x][i].id} | ${review[x][i].date}`);
+                review[x][i].src == null ? $('.review_item>div').eq(i).css({height:0}) :
+                $('.review_item>div').eq(i).css('background-image', `url(${review[x][i].src})`).css('height','130px');
+                $('.review_item>p').eq(i).text(review[x][i].comment);
+            };
+        });
+    };
+    for (let i = 0; i < $('.review_item').length; i++) {
+        $('.review_item>h3').eq(i).text(review[0][i].title)
+        $('.review_item>.review_id').eq(i).text(`${review[0][i].id} | ${review[0][i].date}`);
+        review[0][i].src == null ? $('.review_item>div').eq(i).css({height:0}) :
+            $('.review_item>div').eq(i).css('background-image', `url(${review[0][i].src})`).css('height','130px');
+        $('.review_item>p').eq(i).text(review[0][i].comment);
+
+    };
+    $('.page_number:first').css({fontWeight:'bold',color:'#464B6F'})
+    $('.page_number').click(function(){
+        $(this).siblings().css({fontWeight:'normal',color:'#9e9e9e'})
+        .end().css({fontWeight:'bold',color:'#464B6F'})
+    })
+    document.getElementById('review_total').innerHTML=`총 ${review.length*review[0].length}개`
+    // $('#review_option').change(function(){
+
+    //     console.log($('.review_item>div').eq(2).css('height'))
+    //     console.log($('.review_item>div').eq(2))
+    //     if($('.review_item>div').eq(2).css('height')=='0px'){
+    //     $('.review_item').eq(2).remove()
+    //     }
+
+    // })
+    // let asd=[];
+    // review[0][0]
+    // asd.push(review[0][0])
+    // console.log(asd)
 });
